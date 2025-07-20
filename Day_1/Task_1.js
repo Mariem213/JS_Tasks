62577306ea9f5da99f492c90bad78bff2ba5707a
Mariem M7md <93746564+Mariem213@users.noreply.github.com>
2025-07-20 22:01:10 +0300

    Delete Day_1 directory

--- Day_1/Task_1.js
+++
@@ -1,8 +0,0 @@
-/* ================= Task-1 ================= */
-/* 1.1. Ask the user to enter a message then display it using the different html heading tags
-(from <h1> to <h6>) using Loops. DO NOT write the header element explicitly in your script! */
-
-var msg = prompt("What's your message ?");
-for (var index = 1; index <= 6; index++) {
-    document.writeln("<h" + index + ">" + msg + "</h" + index + ">")
-}
